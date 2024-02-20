708553e6ed718ff64a1ebde261f12bf806fbdd22
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2024-02-20 11:46:55 +0100

    working on it

---
+++ src/components/layout.tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import React, { ReactNode } from "react";
+
+interface AppLayoutProps {
+  title?: string;
+  description?: string;
+  children: ReactNode;
+}
+
+const AppLayout: React.FC<AppLayoutProps> = ({
+  title,
+  description,
+  children,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>
+          {title ? title + "JSON Blog" : "JSON Blog | Lorem ipsum dolorum"}
+        </title>
+        {description && <meta name="description" content={description} />}
+      </Head>
+      <main className="min-h-screen mx-auto container max-w-4xl">
+        {children}
+      </main>
+    </>
+  );
+};
+
+export default AppLayout;
