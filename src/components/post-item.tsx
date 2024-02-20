71f3049be78052d96f078ba3658b677138f89dc5
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2024-02-20 14:41:40 +0100

    getting rid of unused variables

--- src/components/post-item.tsx
+++ src/components/post-item.tsx
@@ -3,7 +3,6 @@ import Link from "next/link";
 import React from "react";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
-import { Badge } from "@/components/ui/badge";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Skeleton } from "@/components/ui/skeleton";
 import useSWR from "swr";
