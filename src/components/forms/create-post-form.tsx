aea0f270ad72cd7c000a4afda5fa38f997fb77b3
Josue Munganga <39856434+josumung999@users.noreply.github.com>
2024-02-21 13:07:39 +0100

    implemented create post

--- src/components/forms/create-post-form.tsx
+++ src/components/forms/create-post-form.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { MinusIcon, PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 import {
   Drawer,
@@ -11,6 +10,21 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "@/components/ui/drawer";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+import { Input } from "../ui/input";
 
 export function CreatePostForm() {
   return (
@@ -26,11 +40,10 @@ export function CreatePostForm() {
               Use the form below to create a post
             </DrawerDescription>
           </DrawerHeader>
-          <div className="">
-            <p>Post form Here</p>
+          <div className="px-4">
+            <CreatePost />
           </div>
           <DrawerFooter>
-            <Button>Submit</Button>
             <DrawerClose asChild>
               <Button variant="outline">Cancel</Button>
             </DrawerClose>
@@ -38,5 +51,88 @@ export function CreatePostForm() {
         </div>
       </DrawerContent>
     </Drawer>
+  );
+}
+
+const FormSchema = z.object({
+  title: z
+    .string({
+      required_error: "Title is required",
+    })
+    .min(10, {
+      message: "Title must be at least 10 characters.",
+    })
+    .max(160, {
+      message: "Title must not be longer than 160 characters.",
+    }),
+  body: z
+    .string({
+      required_error: "Body is required",
+    })
+    .min(200, {
+      message: "Body must be at least 200 characters.",
+    }),
+});
+
+export function CreatePost() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Post Title</FormLabel>
+              <FormControl>
+                <Input placeholder="Give a title to your post" {...field} />
+              </FormControl>
+              <FormDescription>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="body"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Body</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="What's on your mind?"
+                  className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Submit
+        </Button>
+      </form>
+    </Form>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={inter.className}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
+      <Toaster />
     </main>
   );
 }
